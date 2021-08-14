e3c880585f5cfa7f63b13a541e334d4b4659f25e
German Espinosa <germanespinosa@gmail.com>
2021-08-14 13:54:32 -0500

    Add files via upload

---
+++ Source/Experiment/Experiment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Experiment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Experiment, "Experiment" );
---
+++ Source/Experiment/ExperimentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExperimentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPERIMENT_API AExperimentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
